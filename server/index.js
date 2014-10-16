83d8bf15f73ae03a1a327c1979f8f7ec98a66dab
Ivo Tanev <ifch0o69@gmail.com>
2014-10-16 07:19:16 +0300

    some libs and fw added

--- server/index.js
+++ server/index.js
@@ -1,1 +1,7 @@
-var http = require('http');
+var express = require('express'),
+    routes = require('./routes'),
+    port = 8080
+    ;
+
+var app = express();
+console.log(app.get('env'));
---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+// This file contains the site routes.
+
+
+// GET Method
+exports.welcome = function(req, res) {
+
+}
+exports.about = function(req, res) {
+
+}
+exports.apps = function(req, res) {
+
+}
+
+// POST Method
+exports.article = function(req, res) {
+
+}
+
