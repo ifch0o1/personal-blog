270182bd5443a791cdfa9922c301ad2e5d2507fa
Ivo Tanev <ifch0o69@gmail.com>
2014-10-19 02:57:13 +0300

    Skeleton changed, libs added, nothing interesting

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var express = require('express'),
-    routes = require('./routes'),
-    port = 8080
-    ;
+    routes = require('./server/routes'),
+    port = 8080;
 
 var app = express();
 
+
+app.use(express.static(__dirname + '/public'));
+
+//Gets
+app.get('/', routes.home);
+app.get('/about', routes.about);
+app.get('/apps', routes.apps);
+app.get('/article/:id', routes.article);
+//Posts
+
+
+app.listen(port);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,22 @@
 // This file contains the site routes.
-
+var path = require('path');
 
 // GET Method
-exports.welcome = function(req, res) {
-
+exports.home = function(req, res) {
+	var index = path.normalize(__dirname + './../../public/main/index.html');
+	res.sendFile(index);
 }
 exports.about = function(req, res) {
-
+	res.send('<h1>About</h1>');
 }
 exports.apps = function(req, res) {
-
+	res.send('You are in Applications page.');
 }
 
 // POST Method
 exports.article = function(req, res) {
-
+	//Get /:id param from the post url.
+	var id = req.params.id;
+	res.send('You are trying to access article with ID: ' + id);
 }
 
